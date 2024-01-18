e79143c2a06f4e4b08e1dbd42017d5e28fdaa17a
Юрий Асмыкjвич <goker5571@gmail.com>
2024-01-19 05:58:48 +1000

    Добавьте файлы проекта.

---
+++ HomeWorkLib/MyLibrary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeWorkLib
+{
+    public static class MyLibrary
+    {
+       public static void Print()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+    }
+}
---
+++ Homework1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Reflection;
+
+Assembly assembly = Assembly.LoadFrom("HomeWorkLib.dll");
+Type type = assembly.GetType("HomeWorkLib.MyLibrary");
+MethodInfo method = type.GetMethod("Print");
+method.Invoke(null, null);
+
+
+
